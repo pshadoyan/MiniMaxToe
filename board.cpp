c8812db4f5510b2906c3310fb9b57de4c024ad1b
pshadoyan <pshadoyan@gmail.com>
2019-09-26 21:29:28 -0400

    updated win logic

--- board.cpp
+++ board.cpp
@@ -1,9 +1,7 @@
 //
 // Created by Patrick on 9/11/2019.
 //
-
 #include "board.h"
-
 /*
  * Initialize a 3x3 board, all set to 0
  */
@@ -19,21 +17,18 @@ board::board() {
 board::~board() {
     cout << "The End" << endl;
 }
-
 /*
  * Retrieve the board
  */
 vector<vector<int>> board::get_board() {
     return my_board;
 }
-
 /*
 * Change at a specified position
 */
 void board::update_at(int x, int y, int player) {
     my_board[x][y] = player;
 }
-
 /*
  * Print a board? Running out of ideas
  * to make this more complex
@@ -47,7 +42,6 @@ void board::print_board() {
     }
     cout << endl;
 }
-
 /*
  * Wonder if someone won or not
  */
@@ -71,10 +65,17 @@ void board::win() {
     for (int i = 0; i < 3; i++) {
         number = 0;
         for (int j = 0; j < 3; j++) {
+            if(my_board[i][j] == 0)  //If an element is set to 0, there isn't a win on the horizontal
+            {
+                break;
+            }
             number += my_board[i][j];
         }
         if (number == 6) {
             cout << "Winner is player 2" << endl;
+        }
+        if (number == 3) {
+            cout << "Winner is player 1" << endl;
         }
     }
     /*
@@ -83,21 +84,35 @@ void board::win() {
     for (int i = 0; i < 3; i++) {
         number = 0;
         for (int j = 0; j < 3; j++) {
+            if(my_board[i][j] == 0)  //If an element is set to 0, there isn't a win on the vertical
+            {
+                break;
+            }
             number += my_board[j][i];
         }
         if (number == 6) {
             cout << "Winner is player 2" << endl;
+        }
+        if (number == 3) {
+            cout << "Winner is player 1" << endl;
         }
     }
         /*
      * Check all diagonals (y=x | reverse diags (2,0 & 0,2))
      */
     for (int i = 0; i < 3; i++) {
-        number = 0;
+        if(my_board[i][i] == 0) //If an element is set to 0, there isn't a win on the diagonal
+        {
+            break;
+        }
+            number = 0;
             number += my_board[i][i];
         if (number == 6) {
             cout << "Winner is player 2" << endl;
-    }
+        }
+        if (number == 3 && i == 2) {
+            cout << "Winner is player 1" << endl;
+        }
 
     }
 }
